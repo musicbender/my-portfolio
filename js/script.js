b0926d61f270525c58682e9b7da9bb708978f766
musicbender <musicbender@gmail.com>
2016-01-20 18:50:02 -0800

    added pointer and sliding jquery animation to caption

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
     
+    $('.caption').prepend('<svg class="bigTriangleColor caption-triangle" xmlns="http://www.w3.org/2000/svg" version="1.1" width="100%" height="100" viewBox="0 0 100 102" preserveAspectRatio="none"><path d="M0 100 L50 0 L100 100 Z" /></svg>');
     
     
     $('.pic-div').mouseenter(function(){
         var caption = $(this).children().children().eq(1);
-        caption.stop().fadeIn(500);
+        caption.stop().animate({top: '-115px', opacity: '0.7', easing: 'easein'}, 500);
     });
     $('.pic-div').mouseleave(function(){
         var caption = $(this).children().children().eq(1);
-        caption.stop().fadeOut(500);
+        caption.stop().animate({top: '115px', opacity: '0', easing: 'easein'}, 500);
     });
 });
