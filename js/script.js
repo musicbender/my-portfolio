a64f2b5c126a937c03d6c8e1856287c9b4b9ccf0
musicbender <musicbender@gmail.com>
2016-01-27 11:20:52 -0800

    made work section responsive and adjusted header size for smartphone screens

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-    
     $('.caption').prepend('<svg class="bigTriangleColor caption-triangle" xmlns="http://www.w3.org/2000/svg" version="1.1" width="100%" height="100" viewBox="0 0 100 102" preserveAspectRatio="none"><path d="M0 100 L75 0 L100 100 Z" /></svg>');
     
     $('.pic-div').mouseenter(function(){
