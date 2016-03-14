577587e3c89027797c068224e641702ff8bd8457
musicbender <musicbender@gmail.com>
2016-03-13 19:13:59 -0700

    added new files and a gitignore file

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    //*******adds SVG code for work sample items. Removes DRY code from HTML*******//
+    $('.caption').prepend('<svg class="bigTriangleColor caption-triangle" xmlns="http://www.w3.org/2000/svg" version="1.1" width="100%" height="100" viewBox="0 0 100 102" preserveAspectRatio="none"><path d="M0 100 L75 0 L100 100 Z" /></svg>');
+
+    //*******hover effect and caption show/hide for work samples*******//
+    $('.pic-div').mouseenter(function(){
+        var caption = $(this).children().children().eq(1);
+        caption.stop().animate({top: '-115px', opacity: '0.9'}, 350);
+    });
+    $('.pic-div').mouseleave(function(){
+        var caption = $(this).children().children().eq(1);
+        caption.stop().animate({top: '115px', opacity: '0'}, 350);
+    });
+    $('.pic-div').on('click', function() {
+        var caption = $(this).children().children().eq(1);
+        var isHover = $(this).is(":hover");
+
+        caption.stop().animate({top: '115px', opacity: '0'}, 0);
+        if (isHover) {
+            $(this).addClass('pic-div-clicked');
+            $(this).children().children().first().addClass('pic-clicked');
+        }
+    });
+});
+
