67ad2d97923101a7fa94c31eee0ea2c3c99cc79a
Pat Jacobs <musicbender@gmail.com>
2017-02-02 22:36:20 -0800

    added svg backgrounds to replace header and footer images

--- js/main.js
+++ js/main.js
@@ -1,17 +1,26 @@
 $(document).ready(function(){
+
     //*******adds SVG code for work sample items. Removes DRY code from HTML*******//
-    $('.caption').prepend('<svg class="bigTriangleColor caption-triangle" xmlns="http://www.w3.org/2000/svg" version="1.1" width="100%" height="100" viewBox="0 0 100 101" preserveAspectRatio="none"><path d="M0 100 L75 0 L100 100 Z" /></svg>');
+
+    const captionSvg = '<svg class="bigTriangleColor caption-triangle" xmlns="http://www.w3.org/2000/svg" version="1.1" width="100%" height="100" viewBox="0 0 100 101" preserveAspectRatio="none"><path d="M0 100 L75 0 L100 100 Z" /></svg>'
+
+    $('.caption').prepend(captionSvg);
+
 
     //*******hover effect and caption show/hide for work samples*******//
-    $('.pic-div').mouseenter(function(){
+
+    const $pic = $('.pic-div');
+    const $window = $(window);
+
+    $pic.mouseenter(function(){
         var caption = $(this).children().children().eq(1);
         caption.stop().animate({top: '-115px', opacity: '0.9'}, 350);
     });
-    $('.pic-div').mouseleave(function(){
+    $pic.mouseleave(function(){
         var caption = $(this).children().children().eq(1);
         caption.stop().animate({top: '115px', opacity: '0'}, 350);
     });
-    $('.pic-div').on('click', function() {
+    $pic.on('click', function() {
         var caption = $(this).children().children().eq(1);
         var isHover = $(this).is(":hover");
 
@@ -22,18 +31,33 @@ $(document).ready(function(){
         }
     });
 
+
+/*******IF MOBILE, FORCE HOVER ON THUMBS*******/
+    checkSize();
+
+    $window.resize(checkSize());
+
+    function checkSize() {
+        if ($pic.css("z-index") === "11") {
+          var caption = $pic.children().children().eq(1);
+          caption.stop().animate({top: '-115px', opacity: '0.9'}, 350);
+        }
+    }
+
+
+
     //*******Skills Animation*******//
 
-    var skills = $('.tlt');
+    const $skills = $('.tlt');
     if (Modernizr.cssanimations) { //if browser supports it
         skillsAnimate();
     } else {  //if not
-        skills.css('opacity', '1');
+        $skills.css('opacity', '1');
     }
 
     function skillsAnimate () {
-        skills.on('scrollSpy:enter', function() {
-            skills.textillate({
+        $skills.on('scrollSpy:enter', function() {
+            $skills.textillate({
                 initialDelay: 100,
                 in: {
                         effect: 'fadeInUp',
@@ -44,5 +68,5 @@ $(document).ready(function(){
         });
         $('.tlt').scrollSpy();
     }
-});
 
+});
