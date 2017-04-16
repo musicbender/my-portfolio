0da069676611d30ba2fd1608629d928ac3d4c411
Pat Jacobs <musicbender@gmail.com>
2017-04-15 23:08:51 -0700

    alternated triangle toggles on mobile version of demos

--- js/main.js
+++ js/main.js
@@ -6,17 +6,17 @@ $(document).ready(function(){
 
     const captionSvg = '<svg class="bigTriangleColor caption-triangle" xmlns="http://www.w3.org/2000/svg" version="1.1" width="100%" height="100" viewBox="0 0 100 101" preserveAspectRatio="none"><path d="M0 100 L75 0 L100 100 Z" /></svg>';
 
-    const captionSvg2 = '<svg class="bigTriangleColor mobile caption-triangle" xmlns="http://www.w3.org/2000/svg" version="1.1" width="100%" height="100" viewBox="0 0 100 101" preserveAspectRatio="none"><path d="M0 100 L25 0 L100 100 Z" /></svg>';
+    const captionSvg2 = '<svg class="bigTriangleColor caption-triangle mobile" xmlns="http://www.w3.org/2000/svg" version="1.1" width="100%" height="100" viewBox="0 0 100 101" preserveAspectRatio="none"><path d="M0 100 L25 0 L100 100 Z" /></svg>';
 
 
     $caption.prepend(captionSvg);
     $captionMobile.prepend(captionSvg2);
-    $('.bigTriangleColor.mobile').css('display', 'none');
+    $('.caption-triangle.mobile').css('display', 'none');
 
 
 //*******hover effect and caption show/hide for work samples*******//
 
-    const $pic = $('.pic-div');
+    // const $pic = $('.pic-div');
 
     // $pic.on('click', function() {
     //     var caption = $(this).children().children().eq(1);
