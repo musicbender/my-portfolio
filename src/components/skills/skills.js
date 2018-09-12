9e384ee7343ae57182fa0234283d127d71cfb5ee
Pat Jacobs <musicbender@gmail.com>
2018-09-11 22:17:06 -0700

    plish skills

--- src/components/skills/skills.js
+++ src/components/skills/skills.js
@@ -9,7 +9,7 @@ export default (props) => {
       </svg>
       <h2 className="skills-header">Skills</h2>
       <p className="skills tlt">
-          Javascript/ES6 React Redux Node Express MongoDB GraphQL REST PostCSS SCSS LESS AWS nginx jQuery Webpack Rollup UI/Design
+          Javascript/ES6 React Redux Node Express MongoDB GraphQL REST Mocha Jest PostCSS SCSS LESS AWS nginx jQuery Webpack Rollup UI/Design
       </p>
         <button className="button button--wayra button--inverted skills-btn">
           View My <i className="fa fa-github skills-github"></i><a className="skills-btn-a" href="https://www.github.com/musicbender" target="_blank"></a>
