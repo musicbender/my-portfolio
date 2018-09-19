469e318ee2710dddc5f919c78c835c1f92065204
Pat Jacobs <musicbender@gmail.com>
2018-09-18 21:37:05 -0700

    update skills

--- src/components/skills/skills.js
+++ src/components/skills/skills.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './skills.scss';
 
-export default (props) => {
+export default () => {
   return (
     <section className="skills-section">
       <svg className="bigTriangleColor separator-skills" width="100%" height="100" viewBox="0 0 100 102" preserveAspectRatio="none">
@@ -9,7 +9,7 @@ export default (props) => {
       </svg>
       <h2 className="skills-header">Skills</h2>
       <p className="skills tlt">
-          Javascript/ES6 React Redux Node Express MongoDB GraphQL REST Mocha Jest PostCSS SCSS LESS AWS nginx jQuery Webpack Rollup UI/Design
+          Javascript/ES6 React Redux Node Express MongoDB GraphQL REST Next.js Mocha Jest JSS PostCSS SCSS LESS AWS nginx jQuery Webpack Rollup UI/Design
       </p>
         <button className="button button--wayra button--inverted skills-btn">
           View My <i className="fa fa-github skills-github"></i><a className="skills-btn-a" href="https://www.github.com/musicbender" target="_blank"></a>
